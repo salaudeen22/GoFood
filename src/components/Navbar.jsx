84765f2d8a1ddd7570f246e4b8e8cbf1e44e4dee
salaudeen22 <salaudeensalu@gmail.com>
2024-01-21 14:35:00 +0530

    Navbar logic login and logout window done!!

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,75 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 function Navbar() {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("authtoken");
+    navigate("/login");
+  };
   return (
-    <div><nav className="navbar navbar-expand-lg navbar-dark bg-success">
-    <Link className="navbar-brand fs-1 fst-italic" to="#">GoFood</Link>
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-        <li className="nav-item ">
-          <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/login">Login</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/createuser">SignUp</Link>
-        </li>
-       
-       
-      </ul>
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+        <Link className="navbar-brand fs-1 fst-italic" to="#">
+          GoFood
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav me-auto mb-2">
+            <li className="nav-item ">
+              <Link className="nav-link active fs-5" to="/">
+                Home
+              </Link>
+            </li>
+            {localStorage.getItem("authtoken") ? (
+              <li className="nav-item ">
+                <Link className="nav-link active fs-5" to="/">
+                  My orders
+                </Link>
+              </li>
+            ) : (
+              " "
+            )}
+          </ul>
+          {!localStorage.getItem("authtoken") ? (
+            <div className="d-flex ">
+              <Link className="btn bg-white text-success  mx-1" to="/login">
+                Login
+              </Link>
+
+              <Link
+                className="btn bg-white text-success  mx-1"
+                to="/createuser"
+              >
+                SignUp
+              </Link>
+            </div>
+          ) : (
+            <div>
+              <div className="btn btn bg-white text-success mx-2">My Cart</div>
+              <div
+                className="btn btn bg-white text-danger mx-2"
+                onClick={handleLogout}
+              >
+                LogOut
+              </div>
+            </div>
+          )}
+        </div>
+      </nav>
     </div>
-  </nav></div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/screen/Home.jsx
+++ src/screen/Home.jsx
@@ -5,6 +5,7 @@ import Card from "../components/Card";
 import Cousrl from "../components/Cousrl";
 
 export default function Home() {
+  const [search, setseaarch] = useState("");
   const [foodCat, setfoodCat] = useState([]);
   const [foodItem, setfoodItem] = useState([]);
 
@@ -36,40 +37,118 @@ export default function Home() {
         <Navbar />
       </div>
       <div>
-        <Cousrl></Cousrl>
+        <div
+          id="carouselExampleControls"
+          className="carousel slide carousel-fade"
+          data-bs-ride="carousel"
+          style={{ objectFit: "contain !important" }}
+        >
+          <div className="carousel-inner" id="carousel-inner">
+            <div className="carousel-caption" style={{ zIndex: 10 }}>
+              <div className="d-flex justify-content-center">
+                <input
+                  className="form-control mr-sm-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                  value={search}
+                  onChange={(e) => setseaarch(e.target.value)}
+                />
+                {/* <button
+                  className="btn btn-outline-success text-white bg-success"
+                  type="submit"
+                >
+                  Search
+                </button> */}
+              </div>
+            </div>
+            <div className="carousel-item active">
+              <img
+                src="https://source.unsplash.com/random/900×700/?burger"
+                className="d-block w-100"
+                style={{ filter: "brightness(30%)" }}
+                alt="..."
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                src="https://source.unsplash.com/random/900×700/?momo"
+                className="d-block w-100"
+                style={{ filter: "brightness(30%)" }}
+                alt="..."
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                src="https://source.unsplash.com/random/900×700/?pizza"
+                className="d-block w-100"
+                style={{ filter: "brightness(30%)" }}
+                alt="..."
+              />
+            </div>
+          </div>
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleControls"
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleControls"
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
       </div>
 
       <div className="container">
-      {foodCat.length !== 0 ? (
-  foodCat.map((data) => (
-    <div key={data._id} className="row mb-3">
-      <div>
-        <div className="fs-3 m-3">{data.CategoryName}</div>
-      </div>
-      <hr />
-      {foodItem.length !== 0 ? (
-        foodItem
-          .filter((item) => item.CategoryName === data.CategoryName)
-          .map((filteritem) => (
-            <div
-              key={filteritem._id}
-              className="col-12 col-md-6 col-lg-3"
-            >
-           <Card foodName={filteritem.name} options={filteritem.options} imgSrc={filteritem.img} />
-
+        {foodCat.length !== 0 ? (
+          foodCat.map((data) => (
+            <div key={data._id} className="row mb-3">
+              <div>
+                <div className="fs-3 m-3">{data.CategoryName}</div>
+              </div>
+              <hr />
+              {foodItem.length !== 0 ? (
+                foodItem
+                  .filter(
+                    (item) =>
+                      item.CategoryName === data.CategoryName &&
+                      item.name.toLowerCase().includes(search.toLowerCase())
+                  )
+                  .map((filteritem) => (
+                    <div
+                      key={filteritem._id}
+                      className="col-12 col-md-6 col-lg-3"
+                    >
+                      <Card
+                        foodName={filteritem.name}
+                        options={filteritem.options}
+                        imgSrc={filteritem.img}
+                      />
+                    </div>
+                  ))
+              ) : (
+                <div key="noDataFound">"no such data found"</div>
+              )}
             </div>
           ))
-      ) : (
-        <div key="noDataFound">"no such data found"</div>
-      )}
-    </div>
-  ))
-) : (
-  <div key="noResponse">no response</div>
-)}
-
-
-   
+        ) : (
+          <div key="noResponse">no response</div>
+        )}
       </div>
       <div>
         <Footer></Footer>
